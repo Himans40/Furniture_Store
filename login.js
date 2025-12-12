97dd9e5bc93c3c291d26cd9ed1f2e86a3c19b1c6
Himanshu Parmarthi <himanshuparmarthi@Himanshus-MacBook-Pro.local>
2025-12-12 19:47:37 +0530

    adding sidebar on main page

--- login.js
+++ login.js
@@ -1,6 +1,8 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const btn = document.querySelector('.sgn-btn');
+const btn2 = document.querySelector('.sgn2-btn');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
@@ -9,3 +11,13 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+function mainpage(event) {
+    event.preventDefault();
+
+  window.location.href = 'index.html';
+}
+btn.addEventListener('click', mainpage);
+btn2.addEventListener('click', mainpage);
+
+
